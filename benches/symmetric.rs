40ada5361ee272ba661e57ffe3298386f9d511b0
flucium <14249224+flucium@users.noreply.github.com>
2023-09-05 01:35:11 +0900

    update ./benches/symmetric.rs
    added alloc

--- benches/symmetric.rs
+++ benches/symmetric.rs
@@ -1,6 +1,8 @@
 // e.g.
 // cargo bench --package cck --bench symmetric -- --exact --nocapture
+// cargo bench --features=alloc --package cck --bench symmetric -- --exact --nocapture
 // cargo bench --package cck --bench symmetric -- chacha20_poly1305_encrypt_in_place --exact --nocapture
+// cargo bench --features=alloc --package cck --bench symmetric -- chacha20_poly1305_encrypt --exact --nocapture
 
 #![feature(test)]
 extern crate test;
@@ -49,5 +51,40 @@ fn chacha20_poly1305_decrypt_in_place(b: &mut Bencher) {
             .unwrap();
 
         cck::symmetric::chacha20_poly1305_decrypt_in_place(&KEY, &NONCE, &[], &mut message).unwrap()
+    })
+}
+
+#[cfg(feature = "alloc")]
+#[bench]
+fn chacha20_poly1305_encrypt(b: &mut Bencher) {
+    const KEY: [u8; 32] = [
+        57, 175, 86, 245, 102, 95, 243, 137, 254, 235, 187, 7, 87, 88, 175, 190, 102, 82, 188, 163,
+        54, 51, 85, 130, 172, 177, 0, 252, 130, 32, 174, 81,
+    ];
+
+    const NONCE: [u8; 12] = [237, 234, 221, 165, 161, 138, 43, 236, 203, 229, 63, 230];
+
+    b.iter(|| {
+        // hello: [104, 101, 108, 108, 111]
+        cck::symmetric::chacha20_poly1305_encrypt(&KEY, &NONCE, &[], &[104, 101, 108, 108, 111]).unwrap()
+    })
+}
+
+#[cfg(feature = "alloc")]
+#[bench]
+fn chacha20_poly1305_decrypt(b: &mut Bencher) {
+    const KEY: [u8; 32] = [
+        57, 175, 86, 245, 102, 95, 243, 137, 254, 235, 187, 7, 87, 88, 175, 190, 102, 82, 188, 163,
+        54, 51, 85, 130, 172, 177, 0, 252, 130, 32, 174, 81,
+    ];
+
+    const NONCE: [u8; 12] = [237, 234, 221, 165, 161, 138, 43, 236, 203, 229, 63, 230];
+
+    b.iter(move || {
+
+        cck::symmetric::chacha20_poly1305_decrypt(&KEY, &NONCE, &[], &[
+            30, 117, 55, 72, 38, 100, 128, 57, 130, 159, 56, 119, 83, 106, 118, 249, 117, 18,
+            77, 97, 79,
+        ]).unwrap()
     })
 }
