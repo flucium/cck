6e88a993232208b03c11c0cd16c3b7121197d262
flucium <14249224+flucium@users.noreply.github.com>
2023-09-11 02:49:03 +0900

    refactor ./src/lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,10 @@
-mod size;
-pub mod rand;
-pub mod symmetric;
 pub mod asymmetric;
-pub mod hash;
-pub mod format;
 pub mod compress;
-pub type Result<T> = core::result::Result<T,Error>;
-
+pub mod format;
+pub mod hash;
+pub mod rand;
+mod size;
+pub mod symmetric;
+pub type Result<T> = core::result::Result<T, Error>;
 #[derive(Debug,Clone)]
 pub struct Error;
