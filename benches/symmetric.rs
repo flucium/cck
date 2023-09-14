03e74b15548d2f88034a78ef0f9f63b445920479
flucium <14249224+flucium@users.noreply.github.com>
2023-09-14 06:38:05 +0000

    update ./benches/symmetric.rs
    added xchacha20_poly1305 bench

--- benches/symmetric.rs
+++ benches/symmetric.rs
@@ -52,7 +52,6 @@ fn aes_128_gcm_decrypt_in_place(b: &mut Bencher) {
     });
 }
 
-
 #[cfg(feature = "alloc")]
 #[bench]
 fn aes_128_gcm_encrypt(b: &mut Bencher) {
@@ -84,7 +83,6 @@ fn aes_128_gcm_decrypt(b: &mut Bencher) {
         .unwrap();
     })
 }
-
 
 #[bench]
 fn aes_192_gcm_encrypt_in_place(b: &mut Bencher) {
@@ -228,6 +226,80 @@ fn aes_256_gcm_decrypt(b: &mut Bencher) {
             &[
                 166, 194, 44, 81, 34, 139, 144, 143, 127, 98, 255, 206, 166, 169, 47, 171, 239, 57,
                 191, 77, 147,
+            ],
+        )
+        .unwrap();
+    })
+}
+
+#[bench]
+fn xchacha20_poly1305_encrypt_in_place(b: &mut Bencher) {
+    b.iter(|| {
+        let mut message: cck::symmetric::ArrayVec<u8, 256> = cck::symmetric::ArrayVec::new();
+
+        // hello: [104, 101, 108, 108, 111]
+        message
+            .extend_from_slice(&[104, 101, 108, 108, 111])
+            .unwrap();
+
+        cck::symmetric::aead_encrypt_in_place(
+            &cck::symmetric::xchacha20poly1305(&[0u8; 32]),
+            &[0u8; 24],
+            &[],
+            &mut message,
+        )
+        .unwrap();
+    })
+}
+
+#[bench]
+fn xchacha20_poly1305_decrypt_in_place(b: &mut Bencher) {
+    b.iter(|| {
+        let mut message: cck::symmetric::ArrayVec<u8, 256> = cck::symmetric::ArrayVec::new();
+
+        message
+            .try_extend_from_slice(&[
+                16, 251, 250, 229, 138, 249, 8, 112, 183, 173, 62, 24, 90, 50, 250, 154, 190, 134,
+                87, 188, 17,
+            ])
+            .unwrap();
+
+        cck::symmetric::aead_decrypt_in_place(
+            &cck::symmetric::xchacha20poly1305(&[0u8; 32]),
+            &[0u8; 24],
+            &[],
+            &mut message,
+        )
+        .unwrap();
+    })
+}
+
+#[cfg(feature = "alloc")]
+#[bench]
+fn xchacha20_poly1305_encrypt(b: &mut Bencher) {
+    b.iter(|| {
+        // hello: [104, 101, 108, 108, 111]
+        cck::symmetric::aead_encrypt(
+            &cck::symmetric::xchacha20poly1305(&[0u8; 32]),
+            &[0u8; 24],
+            &[],
+            &[104, 101, 108, 108, 111],
+        )
+        .unwrap();
+    })
+}
+
+#[cfg(feature = "alloc")]
+#[bench]
+fn xchacha20_poly1305_decrypt(b: &mut Bencher) {
+    b.iter(|| {
+        cck::symmetric::aead_decrypt(
+            &cck::symmetric::xchacha20poly1305(&[0u8; 32]),
+            &[0u8; 24],
+            &[],
+            &[
+                16, 251, 250, 229, 138, 249, 8, 112, 183, 173, 62, 24, 90, 50, 250, 154, 190, 134,
+                87, 188, 17,
             ],
         )
         .unwrap();
