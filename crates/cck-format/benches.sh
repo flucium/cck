a2d2a4195d425a26ca38812562815fd94dc9301a
flucium <14249224+flucium@users.noreply.github.com>
2023-09-21 15:06:29 +0000

    rm ./crates/cck-format/benches.sh

--- crates/cck-format/benches.sh
+++
@@ -1,3 +0,0 @@
-cargo -q bench --package cck-hash --bench hex -- --exact --nocapture && \
-cargo -q bench --package cck-hash --bench base64ct -- --exact --nocapture && \
-cargo -q bench --package cck-hash --bench pem -- --exact --nocapture
