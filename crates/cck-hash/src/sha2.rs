ba1bd0835a05044c4dacc70a99034da3e63b5d46
flucium <14249224+flucium@users.noreply.github.com>
2023-09-22 16:07:19 +0000

    refactor ./crates/cck-hash/src/sha2.rs
    added doc comment

--- crates/cck-hash/src/sha2.rs
+++ crates/cck-hash/src/sha2.rs
@@ -1,10 +1,23 @@
 use super::Digest;
 use cck_common::size::{SIZE_32, SIZE_64};
 
+/// SHA256
+/// 
+/// # Example
+/// ```
+/// let hash = digest(&[], &[]);
+/// ```
 pub fn digest(bytes: &[u8], salt: &[u8]) -> [u8; SIZE_32] {
     sha256_digest(bytes, salt)
 }
 
+
+/// SHA256
+/// 
+/// # Example
+/// ```
+/// let hash = sha256_digest(&[], &[]);
+/// ```
 pub fn sha256_digest(bytes: &[u8], salt: &[u8]) -> [u8; SIZE_32] {
     let mut hasher = sha2::Sha256::new();
 
@@ -15,6 +28,12 @@ pub fn sha256_digest(bytes: &[u8], salt: &[u8]) -> [u8; SIZE_32] {
     hasher.finalize_reset().into()
 }
 
+/// SHA512
+/// 
+/// # Example
+/// ```
+/// let hash = sha512_digest(&[], &[]);
+/// ```
 pub fn sha512_digest(bytes: &[u8], salt: &[u8]) -> [u8; SIZE_64] {
     let mut hasher = sha2::Sha512::new();
 
@@ -25,6 +44,12 @@ pub fn sha512_digest(bytes: &[u8], salt: &[u8]) -> [u8; SIZE_64] {
     hasher.finalize_reset().into()
 }
 
+/// SHA512_256
+/// 
+/// # Example
+/// ```
+/// let hash = sha512_256_digest(&[], &[]);
+/// ```
 pub fn sha512_256_digest(bytes: &[u8], salt: &[u8]) -> [u8; SIZE_32] {
     let mut hasher = sha2::Sha512_256::new();
 
