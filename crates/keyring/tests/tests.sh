8e592fe0a14f4606b16cdfae45213ff76f2c8c7f
flucium <14249224+flucium@users.noreply.github.com>
2023-09-24 08:01:30 +0000

    update ./crates/keyring/tests/tests.sh
    added string test

--- crates/keyring/tests/tests.sh
+++ crates/keyring/tests/tests.sh
@@ -1,2 +1,4 @@
 cargo -q test --package keyring --test expiry --  --nocapture && \
-cargo -q test --package keyring --test key_type --  --nocapture
+cargo -q test --package keyring --test key_type --  --nocapture && \
+cargo -q test --package keyring --test key --  --nocapture && \
+cargo -q test --package keyring --test string --  --nocapture
