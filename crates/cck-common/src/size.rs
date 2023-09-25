c07513e9d0fadf502b76107573371db865002cee
flucium <14249224+flucium@users.noreply.github.com>
2023-09-25 01:33:22 +0000

    refactor ./crates/cck-common/src/size.rs
    added doc comment

--- crates/cck-common/src/size.rs
+++ crates/cck-common/src/size.rs
@@ -2,17 +2,17 @@
 /// 1024-byte (1 MiB)
 pub const SIZE_1024: usize = 1024;
 
-/// 64-byte
+/// 64-byte (512 bits)
 pub const SIZE_64: usize = 64;
 
-/// 32-byte
+/// 32-byte (256 bits)
 pub const SIZE_32: usize = 32;
 
-/// 24-byte
+/// 24-byte (192 bits)
 pub const SIZE_24: usize = 24;
 
-/// 16-byte
+/// 16-byte (128 bits)
 pub const SIZE_16: usize = 16;
 
-/// 12-byte
+/// 12-byte (96 bits)
 pub const SIZE_12: usize = 12;
