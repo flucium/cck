dd5af9fe76454b8d4f09e2c6dc328312e59e37fa
flucium <14249224+flucium@users.noreply.github.com>
2023-09-27 22:52:54 +0000

    rm ./crates/keyring/tests
    rm -r ./crates/keyring/tests/

--- crates/keyring/tests/tests.sh
+++
@@ -1,4 +0,0 @@
-cargo -q test --package keyring --test expiry --  --nocapture && \
-cargo -q test --package keyring --test key_type --  --nocapture && \
-cargo -q test --package keyring --test key --  --nocapture && \
-cargo -q test --package keyring --test string --  --nocapture
