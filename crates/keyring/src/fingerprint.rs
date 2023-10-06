25de65904956b0d44dfce4dbc169afa212a24253
flucium <14249224+flucium@users.noreply.github.com>
2023-10-06 18:33:43 +0000

    update ./crates/keyring/src/fingerprint.rs

--- crates/keyring/src/fingerprint.rs
+++ crates/keyring/src/fingerprint.rs
@@ -1,19 +1,8 @@
-use cck_common::size::SIZE_64;
-
 /// Use BLAKE3 to generate fingerprint.
-/// 
+///
 /// Returns the fingerprint of the key.
-pub fn blake3_digest(key: &[u8]) -> String {
-    let mut string = String::new();
-
-    string.push_str("blake3:");
-
-    string.push_str(cck_format::hex::encode(
-        &cck_hash::blake3::digest(key, &[]),
-        &mut [0u8; SIZE_64],
-    ));
-
-    string
+pub fn blake3_digest(key: &[u8]) -> Vec<u8> {
+    cck_hash::blake3::digest(key, &[]).to_vec()
 }
 
 // pub fn sha256_digest(key: &[u8]) -> String {
