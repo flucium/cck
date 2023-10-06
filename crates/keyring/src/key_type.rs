2f17bcd38b70dff207c53aec0c6e387eb06d8478
flucium <14249224+flucium@users.noreply.github.com>
2023-10-06 05:37:30 +0000

    update ./crates/keyring/src/key_type.rs

--- crates/keyring/src/key_type.rs
+++ crates/keyring/src/key_type.rs
@@ -16,10 +16,11 @@ pub enum KeyType {
 impl KeyType {
     pub fn from(string: impl Into<String>) -> cck_common::Result<Self> {
         let string = string.into();
-        match string.as_ref() {
-            "Ed25519" => Ok(Self::Ed25519),
-            "X25519" => Ok(Self::X25519),
-            _ => Err(cck_common::Error),
+
+        match string {
+            string if string.eq_ignore_ascii_case("Ed25519") => return Ok(Self::Ed25519),
+            string if string.eq_ignore_ascii_case("X25519") => return Ok(Self::X25519),
+            _ => return Err(cck_common::Error),
         }
     }
 
@@ -50,40 +51,5 @@ impl Display for KeyType {
             Self::Ed25519 => f.write_str("Ed25519"),
             Self::X25519 => f.write_str("X25519"),
         }
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn key_type_default() {
-        let key_type = KeyType::default();
-
-        assert_eq!(key_type, KeyType::Ed25519);
-    }
-
-    #[test]
-    fn key_type_ed25519() {
-        let key_type = KeyType::Ed25519;
-
-        assert_eq!(key_type, KeyType::Ed25519);
-    }
-
-    #[test]
-    fn key_type_x25519() {
-        let key_type = KeyType::X25519;
-
-        assert_eq!(key_type, KeyType::X25519);
-    }
-
-    #[test]
-    fn key_type_to_string() {
-        assert_eq!(KeyType::default().to_string(), "Ed25519");
-
-        assert_eq!(KeyType::Ed25519.to_string(), "Ed25519");
-
-        assert_eq!(KeyType::X25519.to_string(), "X25519");
     }
 }
