7e522898ad4855b92d8dbc897c15d7713e1849a5
flucium <14249224+flucium@users.noreply.github.com>
2023-10-19 05:35:10 +0000

    rm ./crates/cck-format/tests/tests.sh

--- crates/cck-format/tests/tests.sh
+++
@@ -1,3 +0,0 @@
-cargo -q test --package cck-format --test hex --  --nocapture && \
-cargo -q test --package cck-format --test base64ct --  --nocapture && \
-cargo -q test --package cck-format --test pem --  --nocapture
