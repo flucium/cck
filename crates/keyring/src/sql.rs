3bd1e0aec8a47fd33b6fb31a2a7ad05a0eec4237
flucium <14249224+flucium@users.noreply.github.com>
2023-10-19 14:14:10 +0000

    update ./crates/keyring/src/sql.rs
    added SQL_SELECT_FROM_PUBLIC_KEYS_WHERE_USER_ID

--- crates/keyring/src/sql.rs
+++ crates/keyring/src/sql.rs
@@ -37,16 +37,21 @@ SQL statements for the database
 Table: private_keys
 */
 /// Create the table private_keys if it does not exist
-pub const SQL_CREATE_TABLE_PRIVATE_KEYS: &str = "CREATE TABLE IF NOT EXISTS private_keys (user_id TEXT, primary boolean, key_type TEXT, expiry TEXT, private_key BLOB, fingerprint TEXT, signature TEXT);";
+pub const SQL_CREATE_TABLE_PRIVATE_KEYS: &str = "CREATE TABLE IF NOT EXISTS private_keys (user_id TEXT, primary boolean, key_type TEXT, expiry TEXT, private_key BLOB, fingerprint BLOB, signature BLOB);";
 
 /// Drop the table private_keys
 pub const SQL_DROP_TABLE_PRIVATE_KEYS: &str = "DROP TABLE private_keys;";
 
 /// Insert a new private key into the table private_keys
-pub const SQL_INSERT_INTO_PRIVATE_KEYS: &str = "INSERT INTO private_keys (user_id TEXT, primary boolean, key_type TEXT, expiry TEXT, private_key BLOB, fingerprint TEXT, signature TEXT) VALUES(?, ?, ?, ?, ?, ?, ?);";
+pub const SQL_INSERT_INTO_PRIVATE_KEYS: &str = "INSERT INTO private_keys (user_id TEXT, primary boolean, key_type TEXT, expiry TEXT, private_key BLOB, fingerprint BLOB, signature BLOB) VALUES(?, ?, ?, ?, ?, ?, ?);";
 
 /// Select all private keys from the table private_keys
 pub const SQL_SELECT_FROM_PRIVATE_KEYS: &str = "SELECT * FROM private_keys;";
+
+/// Select a private key from the table private_keys where the user_id matches
+/// 
+/// `SQL` - SELECT * FROM private_keys WHERE user_id = ?;
+pub const SQL_SELECT_FROM_PRIVATE_KEYS_WHERE_USER_ID: &str = "SELECT * FROM private_keys WHERE user_id = ?;";
 
 /// Select a private key from the table private_keys where the fingerprint matches
 ///
@@ -65,18 +70,24 @@ SQL statements for the database
 Table: public_keys
 */
 /// Create the table public_keys if it does not exist
-pub const SQL_CREATE_TABLE_PUBLIC_KEYS: &str = "CREATE TABLE IF NOT EXISTS public_keys (user_id TEXT, primary boolean, key_type TEXT, expiry TEXT, public_key BLOB, fingerprint TEXT, signature TEXT);";
+pub const SQL_CREATE_TABLE_PUBLIC_KEYS: &str = "CREATE TABLE IF NOT EXISTS public_keys (user_id TEXT, primary boolean, key_type TEXT, expiry TEXT, public_key BLOB, fingerprint BLOB, signature BLOB);";
 
 /// Drop the table public_keys
 pub const SQL_DROP_TABLE_PUBLIC_KEYS: &str = "DROP TABLE public_keys;";
 
 /// Insert a new public key into the table public_keys
-pub const SQL_INSERT_INTO_PUBLIC_KEYS: &str = "INSERT INTO public_keys (user_id TEXT, primary boolean, key_type TEXT, expiry TEXT, public_key BLOB, fingerprint TEXT, signature TEXT) VALUES(?, ?, ?, ?, ?, ?, ?);";
+pub const SQL_INSERT_INTO_PUBLIC_KEYS: &str = "INSERT INTO public_keys (user_id TEXT, primary boolean, key_type TEXT, expiry TEXT, public_key BLOB, fingerprint BLOB, signature BLOB) VALUES(?, ?, ?, ?, ?, ?, ?);";
 
 /// Select all public keys from the table public_keys
 ///
 /// `SQL` - SELECT * FROM public_keys;
 pub const SQL_SELECT_FROM_PUBLIC_KEYS: &str = "SELECT * FROM public_keys;";
+
+
+/// Select a public key from the table public_keys where the user_id matches
+/// 
+/// `SQL` - SELECT * FROM public_keys WHERE user_id = ?;
+pub const SQL_SELECT_FROM_PUBLIC_KEYS_WHERE_USER_ID: &str = "SELECT * FROM public_keys WHERE user_id = ?;";
 
 /// Select a public key from the table public_keys where the fingerprint matches
 ///
