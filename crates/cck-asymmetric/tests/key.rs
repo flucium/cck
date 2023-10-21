c53f4c638fe1cb04d9c2baec9bc2bdafa87d4786
flucium <14249224+flucium@users.noreply.github.com>
2023-10-21 15:28:40 +0900

    clean ./crates/cck-asymmetric/tests/key.rs

--- crates/cck-asymmetric/tests/key.rs
+++ crates/cck-asymmetric/tests/key.rs
@@ -1,9 +1,9 @@
 use keyring::*;
+
 #[test]
 fn generate_private_key() {
-
     let private_key = keyring::PrivateKey::generate(keyring::KeyType::Ed25519);
-
+    
     assert_eq!(private_key.is_primary(), false);
     assert_eq!(private_key.key_type(), &keyring::KeyType::Ed25519);
     assert_eq!(private_key.as_bytes().len(), 32);
@@ -14,7 +14,6 @@ fn generate_private_key() {
 
 #[test]
 fn generate_public_key() {
-
     let private_key = keyring::PrivateKey::generate(keyring::KeyType::Ed25519);
     let public_key = private_key.public_key();
 
@@ -27,7 +26,6 @@ fn generate_public_key() {
 
 #[test]
 fn generate_private_key_set_primary() {
-
     let mut private_key = keyring::PrivateKey::generate(keyring::KeyType::Ed25519);
 
     assert_eq!(private_key.is_primary(), false);
@@ -39,12 +37,8 @@ fn generate_private_key_set_primary() {
 
 #[test]
 fn is_match_fingerprint() {
-
     let private_key = keyring::PrivateKey::generate(keyring::KeyType::Ed25519);
     let public_key = private_key.public_key();
 
-    assert_eq!(
-        private_key.fingerprint(),
-        public_key.fingerprint()
-    );
+    assert_eq!(private_key.fingerprint(), public_key.fingerprint());
 }
